7e5c01e17ec04a5352c793a8abbfb4cd2b92eb36
Mohamed WLN <114567817+MDDiallo9@users.noreply.github.com>
2023-10-28 16:54:32 +0200

    WIP Tabs

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
 import { key } from "./key"
 
-let forecast = ""
+let tabIndex = 0
+const tabs = []
+// form
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+
+form.addEventListener("submit",(e) => {
+    e.preventDefault()
+    request(key,input.value)
+})
 
 const request = async(key,city) => {
-    forecast = await (await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=7&lang=fr`)).json()
+    let forecast = await (await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=7&lang=fr`)).json()
     /* const json = response.json() */
     console.log(forecast);
     populateCurrent(forecast)
 }
 
-request(key,"Lille")
+/* request(key,"Lille") */
 
 const populateCurrent = (forecast) => {
-    document.querySelector(".location").innerHTML = `<span class="city">${forecast.location.name}</span>, <span class="region">${forecast.location.region}</span>, <span class="country">${forecast.location.country}</span>`
-    document.querySelector(".temps").innerHTML = `<span class="temp_c">${forecast.current["temp_c"]}°C</span> <span class="condition-text">${forecast.current.condition.text}</span>`
+    const container = document.createElement('section');
+    container.setAttribute("data-tab",tabIndex)
+    const location = document.createElement("div")
+    location.innerHTML = `<span class="city">${forecast.location.name}</span>, <span class="region">${forecast.location.region}</span>, <span class="country">${forecast.location.country}</span>`
+    const temps = document.createElement("div")
+    temps.innerHTML = `<span class="temp_c">${forecast.current["temp_c"]}°C</span> <span class="condition-text">${forecast.current.condition.text}</span>`
+    container.append(location,temps)
+    tabIndex += 1
+    const main = document.querySelector('main');
+    main.append(container)
+    tabs.push(container)
+    console.log(tabs);
 }
 
 
