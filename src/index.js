4338d20d9164262127571ff7f892880e7a6e2361
Mohamed WLN <114567817+MDDiallo9@users.noreply.github.com>
2023-10-29 17:12:09 +0100

    Broke tab system , to be remade later

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ let tabIndex = 0;
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const container = document.createElement("section");
-const invisible = document.querySelector("#invisible")
-const main = document.querySelector('main');
+const invisible = document.querySelector("#invisible");
+const main = document.querySelector("main");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -27,22 +27,56 @@ const request = async (key, city) => {
 /* request(key,"Lille") */
 
 const populateCurrent = (forecast, city) => {
-  const container = document.createElement("section");
-  container.setAttribute("data-tab", tabIndex);
-  const location = document.createElement("div");
-  location.innerHTML = `<span class="city">${forecast.location.name}</span>, <span class="region">${forecast.location.region}</span>, <span class="country">${forecast.location.country}</span>`;
+  // City Element
+  const citye = document.createElement("div");
+  citye.id = "city";
+  citye.setAttribute("data-tab", tabIndex);
+  citye.innerHTML = `
+  <div>
+    <div>
+        <p>${forecast.location.name}</p>
+        <p>${forecast.current.condition.text}</p>
+    </div>
+    <p>${forecast.current["temp_c"]}</p>
+  </div>
+  <div></div>
+  `;
+  // Today's forecast element
+  const todaysForecast = document.createElement("div");
+  todaysForecast.id = "todaysforecast";
+  todaysForecast.setAttribute("data-tab", tabIndex);
+    // hour forecast array
+  const hourforecast = forecast.forecast.forecastday[0].hour
+  const hourDiv = document.createElement('div');
+  hourDiv.id = "hourdiv"
+  for (let hour of hourforecast){
+    const hourContainer = document.createElement('div');
+    hourContainer.classList.add("hour")
+    const hourEl = document.createElement('div');
+    hourEl.textContent = `${hour.time.slice(-5)}`
+    const hourImg = document.createElement('div');
+    hourImg.innerHTML = `<img src="${hour.condition.icon}" alt="">`
+    const hourTemp = document.createElement('p');
+    hourTemp.textContent = `${hour.temp_c}°C`
+    hourContainer.append(hourEl,hourImg,hourTemp)
+    hourDiv.append(hourContainer)
+  }
+  todaysForecast.innerHTML = `<h2>Prévisions par heure</h2>`;
+  todaysForecast.append(hourDiv)
+  /* location.innerHTML = `<span class="city">${forecast.location.name}</span>, <span class="region">${forecast.location.region}</span>, <span class="country">${forecast.location.country}</span>`;
   const temps = document.createElement("div");
-  temps.innerHTML = `<span class="temp_c">${forecast.current["temp_c"]}°C</span> <span class="condition-text">${forecast.current.condition.text}</span>`;
-  container.append(location, temps);
+  temps.innerHTML = `<span class="temp_c">${forecast.current["temp_c"]}°C</span> <span class="condition-text">${forecast.current.condition.text}</span>`; */
+  /* container.append(citye);
   invisible.append(container);
   const li = document.createElement("li");
   li.setAttribute("data-tab", tabIndex);
   li.textContent = city;
   const ul = document.querySelector("ul");
-  ul.append(li);
+  ul.append(li); */
+  main.append(citye);
+  main.append(todaysForecast)
   addEvents();
   tabIndex += 1;
-
 };
 
 const addEvents = () => {
@@ -50,11 +84,10 @@ const addEvents = () => {
   for (let li of lis) {
     li.addEventListener("click", (e) => {
       /* cleanContainer(); */
-      invisible.append(main.children[0])
+      invisible.append(main.children[1]);
       const index = e.target.dataset.tab;
       const page = document.querySelector(`section[data-tab="${index}"]`);
-      main.append(page)
+      main.appendChild(page);
     });
   }
 };
-
